4545110c6e9b6909683e79c100330cfffbd7985d
TECHHYB <giri.educom007@gmail.com>
2024-12-01 13:22:34 +0530

    just revise process for practice

---
+++ TVFAPP.java
@@ -0,0 +1,17 @@
+interface TFV
+{      int m=100;
+    
+	TFV()
+	{ 
+	}
+} 
+class MNO extends TFV
+{
+}
+public class TVFAPP
+{
+   public static void main(String x[])
+   {
+	      MNO m = new MNO();
+   }
+}
